bd104f9bdf78fc0959c3e7a24157018397c0964b
gijsvn <69443796+gijsvn@users.noreply.github.com>
2023-05-10 13:40:55 +0200

    Add files via upload

--- data_generation.py
+++ data_generation.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 
 from PIL import Image
 
+import time
+
 class MHSampler:
     def __init__(self, lattice_size = (10, 10), temperature = 293.15, J = 1):
         self.lattice = np.ones(lattice_size)
@@ -19,24 +21,19 @@ class MHSampler:
         non-public function that returns a new total energy corresponding to the
         state of some lattice by looping over all possible pairs of neighbours
         """
-        counted_pairs = []
-
         E = 0
 
         for i in range(lattice.shape[0]):
             for j in range(lattice.shape[1]):
-                if {(i, j), ((i + 1) % lattice.shape[0], j)} not in counted_pairs:
+                #these conditions ensure that no pairs get counted twice
+                if (i + 1) % lattice.shape[0] > i:
                     E -= self.J * lattice[(i, j)] * lattice[((i + 1) % lattice.shape[0], j)]
-                    counted_pairs.append({(i, j), ((i + 1) % lattice.shape[0], j)})
-                if {(i, j), ((i - 1) % lattice.shape[1], j)} not in counted_pairs:
+                if (i - 1) % lattice.shape[0] > i:
                     E -= self.J * lattice[(i, j)] * lattice[((i - 1) % lattice.shape[0], j)]
-                    counted_pairs.append({(i, j), ((i - 1) % lattice.shape[1], j)})
-                if {(i, j), (i, (j + 1) % lattice.shape[1])} not in counted_pairs:
+                if (j + 1) % lattice.shape[0] > j:
                     E -= self.J * lattice[(i, j)] * lattice[(i, (j + 1) % lattice.shape[1])]
-                    counted_pairs.append({(i, j), (i, (j + 1) % lattice.shape[1])})
-                if {(i, j), (i, (j - 1) % lattice.shape[1])} not in counted_pairs:
+                if (j - 1) % lattice.shape[0] > j:
                     E -= self.J * lattice[(i, j)] * lattice[(i, (j - 1) % lattice.shape[1])]
-                    counted_pairs.append({(i, j), (i, (j - 1) % lattice.shape[1])})
 
         return E
 
@@ -56,13 +53,18 @@ class MHSampler:
         return flattened.reshape(shape)
 
     def _mh(self, iterations = None):
+        """
+        function that performs the metropolis hastings algorithm
+        """
         if iterations == None:
             iterations = len(self.lattice.flatten()) * 10
 
+        #main loop
         E0 = self._energy(self.lattice)
         for i in range(iterations):
             new = self._generate_new_state()
             E1 = self._energy(new)
+
             if E1 <= E0:
                 self.lattice = new
                 E0 = E1
@@ -71,6 +73,10 @@ class MHSampler:
                 E0 = E1
 
     def generate_sample(self, temperature, iterations = None):
+        """
+        returns a 'snapshot' of the lattice after performing the Metropolis-Hastings
+        algorithm given a certain temperature
+        """
         self.T = temperature
 
         self._mh(iterations = iterations)
@@ -81,8 +87,8 @@ df = pd.DataFrame(columns = [str(x) for x in range(100)] + ["temperature"])
 
 sampler = MHSampler()
 
-for _ in tqdm(range(500)):
-    temperature = np.random.uniform(0.1, 10)
+for _ in tqdm(range(750)):
+    temperature = np.random.uniform(0.1, 5)
 
     lattice = sampler.generate_sample(temperature)
 
